4a59f3660725031d41f1171ef7a92e4dc2f3a788
Andy Mehalick <andy.mehalick@orangejetpack.com>
2018-08-06 08:00:25 +0300

    add phrases

--- src/Arabique/Api/PhraseController.cs
+++ src/Arabique/Api/PhraseController.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Arabique.Api
 {
@@ -10,8 +10,19 @@ namespace Arabique.Api
     {
         private readonly HashSet<Phrase> _phrases = new HashSet<Phrase>
         {
-            new Phrase("The girl is drinking", "البِنْتُ تَثْرَبُ"),
-            new Phrase("The boy is drinking", "الوَلَدُ يَتَثْرَبُ"),
+            new Phrase("The girl drinks", "البِنْتُ تَشرَبُ"),
+            new Phrase("The boy drinks", "الوَلَدُ يَشرَبُ"),
+            new Phrase("The woman drinks", "المَرْأة تَشرَبُ"),
+            new Phrase("The man drinks", "الرَّجُلُ يَشرَبُ"),
+            new Phrase("She drinks", "هِيَ تَشرَبُ"),
+            new Phrase("He drinks", "هُوَ يَشرَبُ"),
+
+            new Phrase("The girl eats", "البِنْتُ تَأكُلُ"),
+            new Phrase("The boy eats", "الوَلَدُ يَأكُلُ"),
+            new Phrase("The woman eats", "المَرْأة تَأكُلُ"),
+            new Phrase("The man eats", "الرَّجُلُ يَأكُلُ"),
+            new Phrase("She eats", "هِيَ تَأكُلُ"),
+            new Phrase("He eats", "هُوَ يَأكُلُ"),
         };
 
         [HttpGet("random")]
